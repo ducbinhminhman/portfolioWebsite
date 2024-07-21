7ec5eec6faba2e07d36bc0f4e7dba974887700b0
Ryan Man <manbinhminh99@gmail.com>
2024-07-21 20:03:02 +0200

    make the resume mobile compatible

--- pages/resume.js
+++ pages/resume.js
@@ -8,10 +8,17 @@ export default function Resume() {
         <meta name="description" content="Minh's Resume" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-white dark:bg-gray-900 px-10 md:px-20 lg:px-40 py-10">
+      <main className="bg-white dark:bg-gray-900 px-4 sm:px-10 md:px-20 lg:px-40 py-10">
         <section>
-          <h1 className="text-3xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-4xl">Resume</h1>
-          <embed src="/resume.pdf" type="application/pdf" width="100%" height="800px" />
+          <h1 className="text-2xl sm:text-3xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-4xl">Resume</h1>
+          <div className="w-full h-screen">
+            <object data="/resume.pdf" type="application/pdf" width="100%" height="100%">
+              <p className="text-gray-800 dark:text-gray-200">
+                It appears your browser doesn't support PDFs. Please download the PDF to view it: 
+                <a href="/resume.pdf" className="text-teal-600 dark:text-teal-400">Download PDF</a>.
+              </p>
+            </object>
+          </div>
         </section>
       </main>
     </div>
